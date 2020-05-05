9ff96e6bc3d3e857f66d1bc8210386dc101b2a7b
T Connor <tcsvyem+gitkraken@gmail.com>
2020-05-04 23:04:54 -0400

    dev loop rooms and spawns for msg report

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 module.exports.loop = function () {
-    console.log( "Success" );
+    var msg = "Step reacher"
+    for ( var r in Game.rooms ) {
+        var room = Game.rooms[r];
+        msg += "\n" + "r\t" + r + "\t\t" + "room\t" + room;
+        for ( var s in room.spawns ) {
+            var spawn = room.spawns[s];
+            msg += "\n\ts\t" + s + "\t\t" + "spawn\t" + spawn;
+        }
+
+    }
+    console.log( msg );
 }
