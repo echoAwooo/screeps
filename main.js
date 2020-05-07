89bd4c4ecc1b8661e50bdaaaad66b3acd92c362b
T Connor <tcsvyem+gitkraken@gmail.com>
2020-05-06 20:39:09 -0400

    Include test

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
+require( 'include' );
 module.exports.loop = function () {
-    var msg = "Step reacher"
-    for ( var r in Game.rooms ) {
-        var room = Game.rooms[r];
-        msg += "\n" + "r\t" + r + "\t\t" + "room\t" + room;
-        for ( var s in room.spawns ) {
-            var spawn = room.spawns[s];
-            msg += "\n\ts\t" + s + "\t\t" + "spawn\t" + spawn;
-        }
-
-    }
-    console.log( msg );
+    test();
 }
