0632520139bf9e7fc248ddd62856542d314407b4
T Connor <tcsvyem+gitkraken@gmail.com>
2020-05-08 17:51:10 -0400

    Resources constants and store properties isFull isEmpty

--- include.js
+++ include.js
@@ -1,1 +1,16 @@
-function test() { return console.log( "test.include" );}
+require( "_resources" );
+
+Object.defineProperties( Store.prototype, {
+    isFull: {
+        get: function () {
+            if ( this._isFull === undefined ) this._isFull = this.getUsedCapacity() === this.getCapacity();
+            return this._isFull;
+        }
+    },
+    isEmpty: {
+        get: function () {
+            if ( this._isEmpty === undefined ) this._isEmpty = this.getFreeCapacity() === this.getCapacity();
+            return this._isEmpty;
+        }
+    }
+} );
